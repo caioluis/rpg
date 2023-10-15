939cc33500ec3d4cb7183090126dfca2ea41b8a3
Caio Gomes <ocaioluis@gmail.com>
2023-10-15 23:24:11 +0100

    feat(hanko): tweak auth component styles

--- frontend/components/HankoAuth.tsx
+++ frontend/components/HankoAuth.tsx
@@ -1,20 +1,41 @@
 "use client"
 
-import { useEffect } from "react";
-import { register } from "@teamhanko/hanko-elements";
+import { useEffect, useCallback, useState  } from "react";
+import { useRouter } from "next/navigation";
+import { register, Hanko } from "@teamhanko/hanko-elements";
+import "./hanko.css"
 
-const hankoApi = process.env.NEXT_PUBLIC_HANKO_API_URL;
+const hankoApi = process.env.NEXT_PUBLIC_HANKO_API_URL || "";
 
 export default function HankoAuth() {
+    const router = useRouter();
+
+    const [hanko, setHanko] = useState<Hanko>();
+
     useEffect(() => {
-        // @ts-ignore
-        register(hankoApi)
-            .catch((error) => {
-                console.error(error);
-            });
+        import("@teamhanko/hanko-elements").then(({ Hanko }) =>
+            setHanko(new Hanko(hankoApi))
+        );
     }, []);
 
-    return (
-        <hanko-auth />
+    const redirectAfterLogin = useCallback(() => {
+        // successfully logged in, redirect to a page in your application
+        router.replace("/");
+    }, [router]);
+
+    useEffect(
+        () =>
+            hanko?.onAuthFlowCompleted(() => {
+                redirectAfterLogin();
+            }),
+        [hanko, redirectAfterLogin]
     );
+
+    useEffect(() => {
+        register(hankoApi).catch((error) => {
+            console.error(error);
+        });
+    }, []);
+
+    return <hanko-auth />;
 }
