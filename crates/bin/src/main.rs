67aa4bd1b64e035f3da4c0851e9454de777d9861
Caio Gomes <ocaioluis@gmail.com>
2023-10-30 20:51:53 +0000

    build(server): add retry system

--- crates/bin/src/main.rs
+++ crates/bin/src/main.rs
@@ -2,19 +2,31 @@ use anyhow::{
     Context,
     Result,
 };
-
+use tokio::time::{sleep};
 use api::router::Controller;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let db_connection_str = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let mut retries = 0;
+    let max_retries = 5;  // Set your max retries
 
-    let pool = sqlx::postgres::PgPoolOptions::new()
-        .max_connections(50)
-        .acquire_timeout(std::time::Duration::from_secs(30))
-        .connect(&db_connection_str)
-        .await
-        .expect("Failed to connect to Postgres");
+    let pool = loop {
+        match sqlx::postgres::PgPoolOptions::new()
+            .max_connections(50)
+            .acquire_timeout(std::time::Duration::from_secs(30))
+            .connect(&db_connection_str)
+            .await
+        {
+            Ok(pool) => break pool,
+            Err(_e) if retries < max_retries => {
+                retries += 1;
+                eprintln!("Failed to connect to Postgres, retrying... ({} retries left)", max_retries - retries);
+                sleep(std::time::Duration::from_secs(5)).await;
+            },
+            Err(e) => return Err(e.into()),
+        }
+    };
 
     Controller::serve(&pool).await.context("Error while serving the API")?;
     Ok(())
